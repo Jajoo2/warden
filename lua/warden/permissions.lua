d488322061ead68b7844107fc1e1ddf7d6bd5f5b
mei <139186977+Jajoo2@users.noreply.github.com>
2026-03-16 11:32:21 -0500

    Fix typo in permission description for Toolgun

--- lua/warden/permissions.lua
+++ lua/warden/permissions.lua
@@ -34,7 +34,7 @@ end
 Warden.PERMISSION_ALL     = Warden.CreatePermission("whitelist", "Whitelist", "Grants full permissions.", nil, 3)
 Warden.PERMISSION_PHYSGUN = Warden.CreatePermission("physgun", "Physgun", "Allows users to pickup your stuff with the physgun.", nil, 1)
 Warden.PERMISSION_GRAVGUN = Warden.CreatePermission("gravgun", "Gravgun", "Allows users to pickup your stuff with the gravgun.", nil, 1)
-Warden.PERMISSION_TOOL    = Warden.CreatePermission("tool", "Toolgun", "Allows users to use the toogun on your stuff.", nil, 2)
+Warden.PERMISSION_TOOL    = Warden.CreatePermission("tool", "Toolgun", "Allows users to use the toolgun on your stuff.", nil, 2)
 Warden.PERMISSION_USE     = Warden.CreatePermission("use", "Use (E)", "Allows users to sit in your seats, use your wire buttons, etc.", nil, 1)
 Warden.PERMISSION_DAMAGE  = Warden.CreatePermission("damage", "Damage", "Allows users to damage you and your stuff (excluding ACF).", nil, 2)
 
